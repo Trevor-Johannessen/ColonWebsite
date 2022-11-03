c6b9f69c68173083125d9dc32af583cf4efe06b2
Trevor Johannessen <91626575+Trevor-Johannessen@users.noreply.github.com>
2022-11-02 20:48:51 -0400

    Removed scroll, will implement again

--- src/components/Terminal/index.js
+++ src/components/Terminal/index.js
@@ -10,11 +10,10 @@ export default class Terminal extends React.Component{
             grid: [...Array(19)].map(_=>Array(51).fill({text: "", color: "", background: ""})),
             textColor: 'white',
             backgroundColor: 'black',
-            instructions: parser(this.props.instructions),
-            scroll_position: 0
+            instructions: parser(this.props.instructions)
         }
         this.time = 0;
-        ;
+        this.scroll_position = 0;
     }
 
     /*    Create a pixel array to initalize the terminal.
@@ -62,24 +61,12 @@ export default class Terminal extends React.Component{
         return grid;
     }
 
-    redrawGrid(instructions, scroll_position){
-        let grid = [...Array(19)].map(_=>Array(51).fill({text: "", color: "", background: ""}));
+    redrawGrid(instructions, grid){
         for(let index in instructions){
             console.log("Redrawing" + JSON.stringify(instructions[index]))
-            grid = instructions[index].draw(grid, scroll_position)
+            grid = instructions[index].draw(grid, this.scroll_position)
         }
         return grid;
-    }
-
-    scrollPage(difference){
-        this.setState(prevState => ({
-            grid: this.redrawGrid(prevState.instructions, prevState.scroll_position + difference),
-            textColor: prevState.textColor,
-            backgroundColor: prevState.backgroundColor,
-            cursor: prevState.cursor,
-            instructions: prevState.instructions,
-            scroll_position: prevState.scroll_position + difference
-        }))
     }
 
     componentDidUpdate(prevProps, prevState){
@@ -87,12 +74,11 @@ export default class Terminal extends React.Component{
             this.time = 0;
             let parsed_instructions = parser(this.props.instructions);
             this.setState(prevState => ({
-                grid: this.redrawGrid(parsed_instructions),
+                grid: this.redrawGrid(parsed_instructions, [...Array(19)].map(_=>Array(51).fill({text: "", color: "", background: ""}))),
                 textColor: prevState.textColor,
                 backgroundColor: prevState.backgroundColor,
                 cursor: prevState.cursor,
-                instructions: parsed_instructions,
-                scroll_position: prevState.scroll_position
+                instructions: parsed_instructions
             }))
         }
         
@@ -123,7 +109,6 @@ export default class Terminal extends React.Component{
             <div
             id="Terminal"
             key="Terminal"
-            onWheel={this.scrollPage}
             >
             {this.createPixels()}
             
